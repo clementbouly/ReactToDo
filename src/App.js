48fc15117f50ea2c29092a5f251ba52cf407006b
clementboulyComwatt <clement.bouly@comwatt.com>
2019-11-26 11:01:37 +0100

    Transform class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import Task from "./Task";
 import TaskForm from "./TaskForm";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state.taskCount = this.state.tasks.length;
-  }
-  state = {
-    tasks: [
-      { id: 1, content: "coder en react avec le sourire", completed: false },
-      { id: 2, content: "manger une pomme", completed: false },
-      { id: 3, content: "manger une carotte", completed: false }
-    ],
-    taskCount: 0
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "coder en react avec le sourire", completed: false },
+    { id: 2, content: "manger une pomme", completed: false },
+    { id: 3, content: "manger une carotte", completed: false }
+  ]);
+  const [taskCount, setTaskCount] = useState(3);
+
+  const handleDelete = id => {
+    const updatedTasks = [...tasks];
+    console.log(tasks);
+
+    setTasks(updatedTasks.filter(task => task.id !== id));
+    setTaskCount(taskCount-1)
   };
 
-  handleDelete = id => {
-    const tasks = [...this.state.tasks];
+  const handleAdd = task => {
+    const updatedTasks = [...tasks];
+    updatedTasks.push(task);
 
-    this.setState({
-      tasks: tasks.filter(task => task.id !== id),
-      taskCount: this.state.taskCount - 1
-    });
+    setTasks(updatedTasks);
+    setTaskCount(taskCount+1)
   };
 
-  handleAdd = task => {
-    const tasks = [...this.state.tasks];
-    tasks.push(task);
-
-    this.setState({
-      tasks,
-      taskCount: this.state.taskCount + 1
-    });
-  };
-
-  filterBy = param => {
-    const tasks = [...this.state.tasks];
+  const filterBy = param => {
+    const tasks = [...tasks];
 
     switch (param) {
       case "all":
@@ -52,65 +43,60 @@ class App extends React.Component {
     }
   };
 
-  completeTask = id => {
-    const tasks = [...this.state.tasks];
-    const index = tasks.findIndex(task => task.id === id);
-    tasks[index].completed = !this.state.tasks[index].completed;
+  const completeTask = id => {
+    const updatedTasks = [...tasks];
+    const index = updatedTasks.findIndex(task => task.id === id);
+    updatedTasks[index].completed = !updatedTasks[index].completed;
 
-    this.setState({
-      tasks: tasks
-    });
+    setTasks(updatedTasks);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-body">
-          <div className="App-title">
-            <h1>
-              To Do list :
-              <span className="countTask">{this.state.taskCount}</span>
-            </h1>
-          </div>
-          <div className="content">
-            <TaskForm onTaskAdd={this.handleAdd} />
-            <div
-              className="btn-group btn-group-toggle filterBar mt-1 p-2"
-              data-toggle="buttons"
+  return (
+    <div className="App">
+      <div className="App-body">
+        <div className="App-title">
+          <h1>
+            To Do list :<span className="countTask">{taskCount}</span>
+          </h1>
+        </div>
+        <div className="content">
+          <TaskForm onTaskAdd={handleAdd} />
+          <div
+            className="btn-group btn-group-toggle filterBar mt-1 p-2"
+            data-toggle="buttons"
+          >
+            <button
+              className="btn btn-secondary mr-2 filterAll"
+              onClick={() => filterBy("all")}
             >
-              <button
-                className="btn btn-secondary mr-2 filterAll"
-                onClick={() => this.filterBy("all")}
-              >
-                All
-              </button>
-              <button
-                className="btn btn-secondary mr-2 filterAll"
-                onClick={() => this.filterBy("active")}
-              >
-                Active
-              </button>
-              <button className="btn btn-secondary mr-2 filterAll">
-                Completed
-              </button>
-            </div>
-            <div className="App-tasks mt-3">
-              <ul className="list-group list-group-flush ">
-                {this.state.tasks.map(task => (
-                  <Task
-                    key={task.id}
-                    task={task}
-                    onDelete={this.handleDelete}
-                    onComplete={this.completeTask}
-                  />
-                ))}
-              </ul>
-            </div>
+              All
+            </button>
+            <button
+              className="btn btn-secondary mr-2 filterAll"
+              onClick={() => filterBy("active")}
+            >
+              Active
+            </button>
+            <button className="btn btn-secondary mr-2 filterAll">
+              Completed
+            </button>
+          </div>
+          <div className="App-tasks mt-3">
+            <ul className="list-group list-group-flush ">
+              {tasks.map(task => (
+                <Task
+                  key={task.id}
+                  task={task}
+                  onDelete={handleDelete}
+                  onComplete={completeTask}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
